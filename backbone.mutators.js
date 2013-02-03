20ec187ede4fc04f91cb9d2a6f8c2029f168a2e5
Evan Hahn <me@evanhahn.com>
2013-02-03 00:07:30 -0500

    make stuff extend backbone models instead of a new model

--- backbone.mutators.js
+++ backbone.mutators.js
@@ -1,18 +1,17 @@
-Backbone.GSModel = Backbone.Model.extend({
+_.extend(Backbone.Model.prototype, {
+
+	getters: {},
+	setters: {},
 
 	get: function(attr) {
-		// Call the getter if available
 		if (_.isFunction(this.getters[attr])) {
 			return this.getters[attr].call(this);
 		}
-		
 		return Backbone.Model.prototype.get.call(this, attr);
 	},
 
 	set: function(key, value, options) {
 		var attrs, attr;
-
-		// Normalize the key-value into an object
 		if (_.isObject(key) || key == null) {
 			attrs = key;
 			options = value;
@@ -20,19 +19,12 @@ Backbone.GSModel = Backbone.Model.extend({
 			attrs = {};
 			attrs[key] = value;
 		}
-
-		// Go over all the set attributes and call the setter if available
 		for (attr in attrs) {
 			if (_.isFunction(this.setters[attr])) {
 				attrs[attr] = this.setters[attr].call(this, attrs[attr]);
 			}
 		}
-
 		return Backbone.Model.prototype.set.call(this, attrs, options);
-	},
-	
-	getters: {},
-	
-	setters: {}
-	
+	}
+
 });
