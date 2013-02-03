8cd8be91507d0a895cd362023183ee8f60fb84fa
Evan Hahn <me@evanhahn.com>
2013-02-03 00:17:52 -0500

    fix a horrible bug

--- backbone.accessors.js
+++ backbone.accessors.js
@@ -1,30 +1,37 @@
-_.extend(Backbone.Model.prototype, {
+(function() {
 
-	getters: {},
-	setters: {},
+	var oldGet = Backbone.Model.prototype.get;
+	var oldSet = Backbone.Model.prototype.set;
 
-	get: function(attr) {
-		if (_.isFunction(this.getters[attr])) {
-			return this.getters[attr].call(this);
-		}
-		return Backbone.Model.prototype.get.call(this, attr);
-	},
+	_.extend(Backbone.Model.prototype, {
 
-	set: function(key, value, options) {
-		var attrs, attr;
-		if (_.isObject(key) || key == null) {
-			attrs = key;
-			options = value;
-		} else {
-			attrs = {};
-			attrs[key] = value;
-		}
-		for (attr in attrs) {
-			if (_.isFunction(this.setters[attr])) {
-				attrs[attr] = this.setters[attr].call(this, attrs[attr]);
+		getters: {},
+		setters: {},
+
+		get: function(attr) {
+			if (_.isFunction(this.getters[attr])) {
+				return this.getters[attr].call(this);
 			}
-		}
-		return Backbone.Model.prototype.set.call(this, attrs, options);
-	}
+			return oldGet.call(this, attr);
+		},
 
-});
+		set: function(key, value, options) {
+			var attrs, attr;
+			if (_.isObject(key) || key == null) {
+				attrs = key;
+				options = value;
+			} else {
+				attrs = {};
+				attrs[key] = value;
+			}
+			for (attr in attrs) {
+				if (_.isFunction(this.setters[attr])) {
+					attrs[attr] = this.setters[attr].call(this, attrs[attr]);
+				}
+			}
+			return oldSet.call(this, attrs, options);
+		}
+
+	});
+
+})();
